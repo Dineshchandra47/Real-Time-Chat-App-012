5149438e946d62441bcac3d7c8035dff7d4d6d23
Dineshchandra47 <dineshchandraindia5@gmail.com.com>
2023-05-21 23:11:48 +0530

    Commit #7 of Real Time Chat App

--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
 const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
-const app = express
-
+const app = express()
+const authRoutes = require("./routes/auth")
 require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
+
+app.use('/api/auth', authRoutes)
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  console.log('Connected to Database Successfully')
+}).catch((error)=>{
+  console.log(error.message)
+})
+
+
+const server = app.listen(process.env.PORT, ()=>{
+  console.log(`Server is up and running at https://localhost:${process.env.PORT}`)
+})
